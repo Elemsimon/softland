5349bcd8d5177ceef8a46cb47c926bf8de794c03
Elemsimon <elemifesinachisimon@gmail.com>
2025-01-13 09:15:19 +0100

    update

---
+++ src/components/Cards.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <section id="cards" className="cards section">
+      <div className="container">
+        <div className="text-center mb-4 steps-img" data-aos="zoom-out">
+          <img src="/src/assets/img/steps.svg" alt />
+        </div>
+        <div className="row gy-4">
+          <div className="col-lg-4" data-aos="fade-up" data-aos-delay={100}>
+            <div className="card-item">
+              <span>01</span>
+              <h4><a href className="stretched-link">Sign Up</a></h4>
+              <p>Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et consectetur ducimus vero placeat</p>
+            </div>
+          </div>{/* Card Item */}
+          <div className="col-lg-4" data-aos="fade-up" data-aos-delay={200}>
+            <div className="card-item">
+              <span>02</span>
+              <h4><a href className="stretched-link">Repellat Nihil</a></h4>
+              <p>Dolorem est fugiat occaecati voluptate velit esse. Dicta veritatis dolor quod et vel dire leno para dest</p>
+            </div>
+          </div>{/* Card Item */}
+          <div className="col-lg-4" data-aos="fade-up" data-aos-delay={300}>
+            <div className="card-item">
+              <span>03</span>
+              <h4><a href className="stretched-link">Ad ad velit qui</a></h4>
+              <p>Molestiae officiis omnis illo asperiores. Aut doloribus vitae sunt debitis quo vel nam quis</p>
+            </div>
+          </div>{/* Card Item */}
+        </div>
+      </div>
+    </section>
+
+  )
+}
+
+export default Cards
---
+++ src/components/Nav.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <header id="header" className="header d-flex align-items-center fixed-top">
+    <div className="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+
+      <a href="index.html" className="logo d-flex align-items-center">
+        {/* <img src="assets/img/logo.png" alt="" /> */}
+        <h1 className="sitename">SoftLand</h1>
+      </a>
+
+      <nav id="navmenu" className="navmenu">
+        <ul>
+          <li><a href="#hero" className="active">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#features">Features</a></li>
+          <li><a href="#pricing">Pricing</a></li>
+          {/* <li className="dropdown"><a href="#"><span>Dropdown</span> <i className="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="#">Dropdown 1</a></li>
+              <li className="dropdown"><a href="#"><span>Deep Dropdown</span> <i className="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                  <li><a href="#">Deep Dropdown 1</a></li>
+                  <li><a href="#">Deep Dropdown 2</a></li>
+                  <li><a href="#">Deep Dropdown 3</a></li>
+                  <li><a href="#">Deep Dropdown 4</a></li>
+                  <li><a href="#">Deep Dropdown 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Dropdown 2</a></li>
+              <li><a href="#">Dropdown 3</a></li>
+              <li><a href="#">Dropdown 4</a></li>
+            </ul>
+          </li> */}
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+        <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+
+    </div>
+  </header>
+  )
+}
+
+export default Nav
